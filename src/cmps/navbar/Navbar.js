ca739798ee5e72774e1fa929920a57760fb62834
Alina <alina.boshkov@gmail.com>
2020-01-15 16:05:05 +0200

    small updates

--- src/cmps/navbar/Navbar.js
+++ src/cmps/navbar/Navbar.js
@@ -1,28 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+// update to use NavLink 
 export default class Navbar extends Component {
- render(){
-     const noLoggedinUser=false
-     const userName=  (!noLoggedinUser)?'guest':''
+  render() {
+    const noLoggedinUser = false
+    const userName = (!noLoggedinUser) ? 'guest' : ''
     return (
-    <div className="navbar flex">
-      <Link className="navbar link" to="/">
-        Company Name
-      </Link>
-      <div className="flex">
+      <div className="navbar flex">
         <Link className="navbar link" to="/">
-          Home Page
+          Company Name
+      </Link>
+        <div className="flex">
+          <Link className="navbar link" to="/">
+            Home Page
         </Link>
-        <Link className="navbar link" to="/games">
-          Explore
+          <Link className="navbar link" to="/games">
+            Explore
         </Link>
-        <Link className="navbar link" to={`/user/${userName}`}>
-          profile
+          <Link className="navbar link" to={`/user/${userName}`}>
+            profile
         </Link>
-
+        </div>
       </div>
-    </div>
-  )
- }
+    )
+  }
 }
