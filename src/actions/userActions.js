2677af7a149b381c061fb33768de25077b50e4d4
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-01-17 11:39:44 +0200

    reducers fix

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -3,28 +3,27 @@ import userService from "../services/userService";
 
 function login(user) {
     return {
-        type: 'SET_GAMES',
+        type: 'LOGIN',
         user
     }
 }
-
 
 export function login(cred) {
     return async dispatch => {
         try {
             const user = await userService.login(cred);
-            dispatch('LOGIN',(user));
+            dispatch(login(user));
 
         } catch (err) {
         }
     };
 }
 
-export function login(cred) {
+export function signup(cred) {
     return async dispatch => {
         try {
             const user = await userService.signup(cred);
-            dispatch('LOGIN',(user));
+            dispatch(login(user));
 
         } catch (err) {
         }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,21 +1,20 @@
 const INITIAL_STATE = {
     logedinUser: {},
-  };
-  
-  export default function gameReducer(state = INITIAL_STATE, action) {
+};
+
+export default function userReducer(state = INITIAL_STATE, action) {
     switch (action.type) {
-      case "LOGIN":
-        return {
-          ...state,
-          logedinUser: action.user
-        };
-      case "LOGOUT":
-        return {
-          ...state,
-          logedinUser: {}
-        };
-      default:
-        return state;
+        case "LOGIN":
+            return {
+                ...state,
+                logedinUser: action.user
+            };
+        case "LOGOUT":
+            return {
+                ...state,
+                logedinUser: {}
+            };
+        default:
+            return state;
     }
-  }
-  
+}
