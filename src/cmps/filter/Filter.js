521bd9b1aa94218f481380030815860095ccfd4d
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-01-27 15:59:00 +0200

    changed filter

--- src/cmps/filter/Filter.js
+++ src/cmps/filter/Filter.js
@@ -5,26 +5,39 @@ import './_Filter.scss'
 export default class Filter extends Component {
     state = {
         title: '',
-        tag: ''
+        tag: '',
+        filterBy: 'title'
     }
 
     inputChange = ev => {
         let fieldName = ev.target.name;
-        this.setState({ [fieldName]: ev.target.value });
+        if (fieldName === 'filterBy') {
+            return this.setState({ title: '', tag: '', filterBy: ev.target.value }, () => this.onFilterBy())
+        }
+        this.setState({ [this.state.filterBy]: ev.target.value }, () => this.onFilterBy());
     };
 
-    onFilterBy = (ev) => {
-        const value = ev.target.value;
-        const name = ev.target.name;
-        this.setState({ [name]: value }, () => this.props.onFilterBy(this.state))
+    onFilterBy = () => {
+        this.props.onFilterBy(this.state)
     }
 
 
     render() {
         const { sortByPrice, sortByDownloads } = this.props
-        return <div className="search-container">
-            <input className="search-input input" name='title' type="search" placeholder="Search" onChange={this.onFilterBy} />
-            <input className="search-input input" name='tag' type='text' placeholder='Search by Tags' onChange={this.onFilterBy} />
+        const { filterBy } = this.state
+        return <div className="search-container align-center flex">
+            <input className="search-input input" value={this.state[this.state.filterBy]}
+                type="search" placeholder={`Search by ${filterBy}`} onChange={this.inputChange} />
+            <div className='flex column'>
+                <input className='hidden' name='filterBy' onChange={this.inputChange} defaultChecked id='title' value='title' type='radio' />
+                <label className='btn title-btn pointer' htmlFor='title'>
+                    title
+             </label>
+                <input className='hidden' onChange={this.inputChange} name='filterBy' id='tag' value='tag' type='radio' />
+                <label className='btn tag-btn pointer' htmlFor='tag'>
+                    tag
+             </label>
+            </div>
             <span>Sort by: </span>
             <button className='btn filter-btn' onClick={sortByDownloads}>Popular</button>
             <button className='btn filter-btn' onClick={sortByPrice}>Price</button>
