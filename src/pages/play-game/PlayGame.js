3c4576fbf9e6e64de6a2a1bee679a0801a1d9e29
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-01-28 17:05:22 +0200

    fixed play

--- src/pages/play-game/PlayGame.js
+++ src/pages/play-game/PlayGame.js
@@ -8,7 +8,7 @@ class PlayGame extends Component {
     state = { comments: null }
 
     componentDidMount() {
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
         let userName = (this.props.loggedInUser) ? this.props.loggedInUser.userName : 'Guest';
         SocketService.setup()
         SocketService.emit('chat room', this.props.match.params.id);
@@ -35,9 +35,9 @@ class PlayGame extends Component {
         let addedComments
         const logInMsg = !this.props.loggedInUser ? 'Buy the game to enjoy the game fully' : '';
         addedComments = <Comments user={this.props.loggedInUser} onAddCommentOrReview={this.sendComment} comments={comments} />
-        return (<div className='content-container'>
+        return (<div className='content-container container'>
             <h3>{logInMsg}</h3>
-            <div className= 'game-content-container flex'>
+            <div className='game-content-container flex'>
                 <iframe title="play" src="https://www.gameflare.com/embed/cartoon-strike/" frameBorder="0" scrolling="no" width="1000" height="635" allowFullScreen></iframe>
                 <div className='comments-container'>{addedComments}</div>
             </div>
